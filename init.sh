bf1768528b7a1b17a4bddd02cdd3f204c62324d0
Willberg <hszhongtao@163.com>
2021-04-01 17:18:37 +0800

    fix

--- init.sh
+++ init.sh
@@ -3,7 +3,6 @@
 mkdir -p /home/build
 mkdir -p /home/logs/kunkka
 mkdir -p /home/server
-mkdir -p /home/resource/kunkka
 mkdir -p /home/code
 mkdir -p /home/logs/kunkka/40001
 mkdir -p /home/logs/kunkka/40002
@@ -11,3 +10,12 @@ mkdir -p /home/logs/kunkka/40002
 # 克隆代码
 cd /home/code && git clone https://github.com/Willberg/kunkka.git
 cd /home/code && git clone https://github.com/Willberg/lina.git
+
+# 拷贝kunkka配置
+mv -r resource /home
+
+#拷贝nginx配置
+mv conf/nginx.conf /home/server/nginx/conf
+
+# 启动nginx
+nginx
--- nginx.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-#拷贝nginx配置
-mv conf/nginx.conf /home/server/nginx/conf
-
-# 启动nginx
-nginx
